a2fcdf66b2f3f9badb0d2f66646e53e19773ae7b
Gururajan4878 <rgururajan4878@gmail.com>
2025-08-10 19:33:49 +0530

    first commit1

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+   baseURL: 'https://cosmetology-backend-xxuu.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
